98a4e8da8dd2e8eb87047f6048d753d6cc2baa0b
Ethan Britt <eb7931@gmail.com>
2020-02-27 18:53:33 -0600

    Added insert method to AVL_Tree
    Made insert capable of insertion using recursion, can handle empty tree,
    moving down branches properly, setting height as the stack is collapsed.
    Needs tested
    Need to add balancing function

--- CS3345_Project2/src/AVL_Tree.java
+++ CS3345_Project2/src/AVL_Tree.java
@@ -9,10 +9,43 @@
  * is affected in height/balance from the insertion
  */
 
+/*
+ * I will be determining order of the tree using compareTo since it
+ * is a requirement that the key be a string here. 
+ * String1.compareTo(String2) < 0 will indicate that String1 < String2
+ * String1.compareTo(String2) > 0 will indicate that String1 > String2
+ */
 public class AVL_Tree{
-	public void insert() {
+	private AVLNode head = null;
+	public void insert(Book b) {
+		balancedInsert(head, new AVLNode(b));
 	}	
-			
+	private void balancedInsert(AVLNode parent, AVLNode b) {
+		if(parent == null) {
+			parent = b;
+		}
+		else if(parent.key.compareTo(b.key) < 0) {
+			balancedInsert(parent.leftPtr(), b);
+		}
+		else if(parent.key.compareTo(b.key) > 0) {
+			balancedInsert(parent.rightPtr(), b);
+		}
+		if(parent.getHeight() == max(parent.leftHeight(), parent.rightHeight())) {
+			parent.setHeight(parent.getHeight() + 1);
+		}
+		balance(parent);
+	}
+	private void balance(AVLNode b) {
+		
+	}
+	private int max(int a, int b) {
+		if(a <= b) {
+			return b;
+		}
+		else {
+			return a;
+		}
+	}
 	/*
 	 * AVLNode is added as a class inside the tree as it is a 
 	 * component of the tree, where the tree is accessed in order 
@@ -23,8 +56,14 @@ public class AVL_Tree{
 		String key; // (ISBN number)
 		Book value; //create a class representing a book with minimum attributes
 		int height;
-		AVLNode leftPtr;
-		AVLNode rightPtr;
+		private AVLNode leftPtr;
+		private AVLNode rightPtr;
+		public AVLNode rightPtr() {
+			return rightPtr;
+		}
+		public AVLNode leftPtr() {
+			return leftPtr;
+		}
 		public AVLNode(Book b) {
 			value = b;
 			key = value.key();
@@ -32,14 +71,28 @@ public class AVL_Tree{
 			leftPtr = null;
 			rightPtr = null;
 		}
-		
+		public int leftHeight() {
+			if(leftPtr == null) {
+				return -1;
+			}
+			else {
+				return leftPtr.getHeight();
+			}
+		}
+		public int rightHeight() {
+			if(rightPtr == null) {
+				return -1;
+			}
+			else {
+				return rightPtr.getHeight();
+			}
+		}
 		public int getHeight() {
 			return height;
 		}
 		public void setHeight(int h){
 			height = h;
 		}
-		
 	}
 		
 			
